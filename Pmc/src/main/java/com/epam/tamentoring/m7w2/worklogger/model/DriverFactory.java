b43d2f4197340e88132c2fbcfb97577c151785f4
Gergely_Glosz <ggeri74@gmail.com>
2016-05-09 11:27:22 +0200

    m7w3

--- Pmc/src/main/java/com/epam/tamentoring/m7w2/worklogger/model/DriverFactory.java
+++ Pmc/src/main/java/com/epam/tamentoring/m7w2/worklogger/model/DriverFactory.java
@@ -1,10 +1,31 @@
 package com.epam.tamentoring.m7w2.worklogger.model;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
+
+    private static WebDriver driver = null;
+    private static Dimension dimension = new Dimension(1600, 600);
+
     public static WebDriver getDriver() {
-        return new FirefoxDriver();
+        driver = new FirefoxDriver();
+        driver.manage().window().setSize(dimension);
+        return driver;
+    }
+
+    public static WebDriver getRemoteWebDriver() {
+        try {
+            driver = new RemoteWebDriver(new URL("http://visko.dyndns.hu:61983/wd/hub"), DesiredCapabilities.firefox());
+            driver.manage().window().setSize(dimension);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return driver;
     }
 }
---
+++ Pmc/src/main/java/com/epam/tamentoring/m7w3/trello/pageobjects/Page.java
@@ -0,0 +1,7 @@
+package com.epam.tamentoring.m7w3.trello.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+class Page {
+    WebDriver driver;
+}
